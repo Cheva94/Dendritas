a43ad6b7564b2be52c4a2e3faa1cffb342bea0b3
muriel-z <72110577+muriel-z@users.noreply.github.com>
2022-02-12 21:31:46 -0300

    Resuelvo el problema de voltaje del Landau con el metodo de diferencia finita

---
+++ V_test.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 12 18:39:00 2022
+
+@author: Muris
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+
+#Pruebita del ejercicio de voltaje del Landau
+
+N_iter=1000
+
+Nx = 100
+Ny = 100
+
+malla = np.zeros([Nx+1,Ny+1])
+
+#Condiciones de borde
+
+malla[:,0] = 100
+
+for k in range(N_iter): 
+    for i in range(1,Nx,1):
+        for j in range(1,Ny,1):
+            malla[i,j] = (malla[i+1,j]+malla[i-1,j]+malla[i,j+1]+malla[i,j-1])/4 
+            
+#Grafico
+plt.figure(10)
+plt.pcolormesh(malla[:,:])
+plt.xlabel('y [mm]')
+plt.ylabel('z [mm]')
+ 
+### Gráfico de superficie
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+# Make data.
+X = np.arange(0, 101, 1)
+Y = np.arange(0, 101, 1)
+Z = malla
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(0, 100)
+ax.zaxis.set_major_locator(LinearLocator(10))
+# A StrMethodFormatter is used automatically
+ax.zaxis.set_major_formatter('{x:.02f}')
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
