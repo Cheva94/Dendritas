930c0155affb149ef653108953cc94ed9080a2bf
muriel-z <72110577+muriel-z@users.noreply.github.com>
2022-02-14 12:41:48 -0300

    en dendritas_07.py intento calcular el campo eléctrico

--- V_test.py
+++ V_test.py
@@ -33,26 +33,5 @@ plt.pcolormesh(malla[:,:])
 plt.xlabel('y [mm]')
 plt.ylabel('z [mm]')
  
-### Gráfico de superficie
-
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-
-# Make data.
-X = np.arange(0, 101, 1)
-Y = np.arange(0, 101, 1)
-Z = malla
-
-# Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-
-# Customize the z axis.
-ax.set_zlim(0, 100)
-ax.zaxis.set_major_locator(LinearLocator(10))
-# A StrMethodFormatter is used automatically
-ax.zaxis.set_major_formatter('{x:.02f}')
-
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
 
 plt.show()
