ba94131f8f6bbf1b1f3ea6fd7037dfbd5b855d37
Cheva <nacho.cheva@gmail.com>
2022-05-24 20:52:09 +0200

    3d plot

--- v.0-partida/out/plot.py
+++ v.0-partida/out/plot.py
@@ -4,56 +4,62 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from cycler import cycler
+# from mpl_toolkits.mplot3d import Axes3D
 
 plt.rcParams["font.weight"] = "bold"
-plt.rcParams["font.size"] = 35
-
+plt.rcParams["font.size"] = 15
+#
 plt.rcParams["axes.labelweight"] = "bold"
-plt.rcParams["axes.linewidth"] = 3
+plt.rcParams["axes.linewidth"] = 2
 plt.rcParams["axes.prop_cycle"] = cycler('color', ['tab:orange', 'mediumseagreen', 'm', 'y', 'k'])
-
-plt.rcParams['xtick.major.size'] = 10
-plt.rcParams['xtick.major.width'] = 3
-plt.rcParams['ytick.major.size'] = 10
-plt.rcParams['ytick.major.width'] = 3
-
-plt.rcParams["legend.loc"] = 'best'
+#
+# plt.rcParams['xtick.major.size'] = 10
+# plt.rcParams['xtick.major.width'] = 3
+# plt.rcParams['ytick.major.size'] = 10
+# plt.rcParams['ytick.major.width'] = 3
+#
+# plt.rcParams["legend.loc"] = 'best'
 plt.rcParams["legend.frameon"] = True
 plt.rcParams["legend.fancybox"] = True
-plt.rcParams["legend.fontsize"] = 20
+plt.rcParams["legend.fontsize"] = 10
 plt.rcParams["legend.edgecolor"] = 'black'
-
-plt.rcParams["figure.figsize"] = 25, 10
-plt.rcParams["figure.autolayout"] = True
-
-plt.rcParams["lines.linewidth"] = 4
-plt.rcParams["lines.markersize"] = 8
-plt.rcParams["lines.linestyle"] = ':'
+#
+# plt.rcParams["figure.figsize"] = 12.5, 10
+# plt.rcParams["figure.autolayout"] = True
+#
+# plt.rcParams["lines.linewidth"] = 4
+# plt.rcParams["lines.markersize"] = 8
+# plt.rcParams["lines.linestyle"] = ':'
 
 def main():
 
     Dep_init = pd.read_csv("Est0_Dep.csv").to_numpy()
     Lib_init = pd.read_csv("Est0_Lib.csv").to_numpy()
-    Dep_end = pd.read_csv("Est1_Dep.csv").to_numpy()
-    Lib_end =  pd.read_csv("Est1_Lib.csv").to_numpy()
+    # Dep_end = pd.read_csv("Est1_Dep.csv").to_numpy()
+    # Lib_end =  pd.read_csv("Est1_Lib.csv").to_numpy()
 
-    fig, (axi, axf) = plt.subplots(1,2)
+    fig = plt.figure()
+    # fig.set_size_inches(6, 6)
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(Dep_init[:, 0], Dep_init[:, 1], Dep_init[:, 2], label='Li$^0$')
+    ax.scatter(Lib_init[:, 0], Lib_init[:, 1], Lib_init[:, 2], label='Li$^+$')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.legend()#loc='center left', bbox_to_anchor=(1, 0.5), ncol = 1)
+    ax.grid(False)
+    ax.set_title('Estado Inicial')
+    fig.set_size_inches(10, 10)
 
-    axi.scatter(Lib_init[:, 0], Lib_init[:, 1], label='Li$^+$')
-    axi.scatter(Dep_init[:, 0], Dep_init[:, 1], label='Li$^0$')
-    axi.set_xlabel('x')
-    axi.set_ylabel('y')
-    axi.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol = 1)
-    axi.set_title('Estado Inicial')
-
-    axf.scatter(Lib_end[:, 0], Lib_end[:, 1], label='Li$^+$')
-    axf.scatter(Dep_end[:, 0], Dep_end[:, 1], label='Li$^0$')
-    axf.set_xlabel('x')
-    axf.set_ylabel('y')
-    axf.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol = 1)
-    axf.set_title('Estado Final')
+    # axf.scatter(Lib_end[:, 0], Lib_end[:, 1], label='Li$^+$')
+    # axf.scatter(Dep_end[:, 0], Dep_end[:, 1], label='Li$^0$')
+    # axf.set_xlabel('x')
+    # axf.set_ylabel('y')
+    # axf.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol = 1)
+    # axf.set_title('Estado Final')
 
     plt.savefig('Dendritas')
+    plt.show()
 
 if __name__ == "__main__":
     main()
