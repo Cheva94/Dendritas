4697e6283ff0afb4217c4eaf427b556ed83de3fd
Cheva <nacho.cheva@gmail.com>
2022-05-26 22:41:08 +0200

    NVOS PARAMS

--- core.c
+++ core.c
@@ -12,13 +12,13 @@ void init(double* lib, double* dep)
     FILE *f_initLib;
     f_initLib = fopen("Est0_Lib.csv", "w");
 
-    for (i = 0; i < 2 * NM; i += 2) { // NM it
-        *(lib + i + 0) = LONGX * rand() / (double)RAND_MAX; // prod div >> 2
-        *(lib + i + 1) = LONGY * rand() / (double)RAND_MAX; // prod div >> 2
-    } // flop = 4 * NM
+    for (i = 0; i < 2 * NL; i += 2) { // NL it
+        *(lib + i + 0) = LX * rand() / (double)RAND_MAX; // prod div >> 2
+        *(lib + i + 1) = LY * rand() / (double)RAND_MAX; // prod div >> 2
+    } // flop = 4 * NL
 
     fprintf(f_initLib, "x, y\n");
-    for (i = 0; i < 2 * NM; i += 2) {
+    for (i = 0; i < 2 * NL; i += 2) {
         fprintf(f_initLib, "%f, %f\n", *(lib + i + 0), *(lib + i + 1));
     }
     fclose(f_initLib);
@@ -26,17 +26,17 @@ void init(double* lib, double* dep)
     FILE *f_initDep;
     f_initDep = fopen("Est0_Dep.csv", "w");
 
-    for (i = 0; i < N0; i++) { // N0 it
-        *(dep + idx + 0) = i * DATT; // prod >> 1
+    for (i = 0; i < ND; i++) { // ND it
+        *(dep + idx + 0) = i * DAT; // prod >> 1
         idx += 2; // sum >> 1
-    } // flop = 2 * N0
+    } // flop = 2 * ND
 
     fprintf(f_initDep, "x, yCat, yAn\n");
-    for (i = 0; i < 2 * N0; i += 2) {
-        fprintf(f_initDep, "%f, %f, %f\n", *(dep + i + 0), *(dep + i + 1), *(dep + i + 1) + LONGY);
+    for (i = 0; i < 2 * ND; i += 2) {
+        fprintf(f_initDep, "%f, %f, %f\n", *(dep + i + 0), *(dep + i + 1), *(dep + i + 1) + LY);
     }
     fclose(f_initDep);
-} // flop = 4 * NM + 2 * N0
+} // flop = 4 * NL + 2 * ND
 
 double pbc(double coord, const double cell_length)
 {
@@ -70,16 +70,16 @@ double neutral(double* lib, double* dep, int* count, const int j)
 
         flop += 6;
 
-        if (dist2 < DATT2) { // comp >> 1
+        if (dist2 < DAT2) { // comp >> 1
             dist = sqrt(dist2); // sqrt >> 1
 
-            *(dep + 2 * (*count) + 0) = pbc(distx * DATT / dist + *(dep + k + 0), LONGX); // mul div sum pbc >> 5
-            *(dep + 2 * (*count) + 1) = rbc(disty * DATT / dist + *(dep + k + 1), LONGY); // mul div sum rbc >> 6
+            *(dep + 2 * (*count) + 0) = pbc(distx * DAT / dist + *(dep + k + 0), LX); // mul div sum pbc >> 5
+            *(dep + 2 * (*count) + 1) = rbc(disty * DAT / dist + *(dep + k + 1), LY); // mul div sum rbc >> 6
 
             *count += 1; // sum >> 1
 
-            *(lib + j + 0) = LONGX * rand() / (double)RAND_MAX; // prod div >> 2
-            *(lib + j + 1) = LONGY * rand() / (double)RAND_MAX; // prod div >> 2
+            *(lib + j + 0) = LX * rand() / (double)RAND_MAX; // prod div >> 2
+            *(lib + j + 1) = LY * rand() / (double)RAND_MAX; // prod div >> 2
 
             flop += 17;
         } // flop dentro del if = 17
@@ -91,7 +91,7 @@ void move(double* lib, double* dep, int* count, double* flopGral)//, double* flo
 {
     double tita, gx, gy;
 
-    for (int j = 0; j < 2 * NM; j += 2) { // NM it
+    for (int j = 0; j < 2 * NL; j += 2) { // NL it
         tita = 2 * M_PI * rand() / (double)RAND_MAX; // prod div >> 3
         gx = cos(tita); // cos >> 1
         gy = sin(tita); // sin >> 1
@@ -99,14 +99,14 @@ void move(double* lib, double* dep, int* count, double* flopGral)//, double* flo
         *(lib + j + 0) += Q * gx; // prod sum >> 2
         *(lib + j + 1) += Q * gy; // prod sum >> 2
 
-        *(lib + j + 0) = pbc(*(lib + j + 0), LONGX); // pbc >> 2
-        *(lib + j + 1) = rbc(*(lib + j + 1), LONGY); // rbc >> 3
+        *(lib + j + 0) = pbc(*(lib + j + 0), LX); // pbc >> 2
+        *(lib + j + 1) = rbc(*(lib + j + 1), LY); // rbc >> 3
 
         (*flopGral) += 14;
 
         (*flopGral) += neutral(lib, dep, count, j); // count * 6 + if * 17
     }
-} // flop = NM * (14 + count * 6 + if * 17)
+} // flop = NL * (14 + count * 6 + if * 17)
 
 void end(double* lib, double* dep, double tSim, double wall, double* flopGral)//, double* flopNeu)
 {
@@ -116,7 +116,7 @@ void end(double* lib, double* dep, double tSim, double wall, double* flopGral)//
     f_endDep = fopen("Est1_Dep.csv", "w");
 
     fprintf(f_endDep, "x, y\n");
-    for (i = 0; i < 2 * N0MAX; i += 2) {
+    for (i = 0; i < 2 * NMAX; i += 2) {
         fprintf(f_endDep, "%f, %f\n", *(dep + i + 0), *(dep + i + 1));
     }
     fclose(f_endDep);
@@ -125,7 +125,7 @@ void end(double* lib, double* dep, double tSim, double wall, double* flopGral)//
     f_endLib = fopen("Est1_Lib.csv", "w");
 
     fprintf(f_endLib, "x, y\n");
-    for (i = 0; i < 2 * NM; i += 2) {
+    for (i = 0; i < 2 * NL; i += 2) {
         fprintf(f_endLib, "%f, %f\n", *(lib + i + 0), *(lib + i + 1));
     }
     fclose(f_endLib);
@@ -134,15 +134,14 @@ void end(double* lib, double* dep, double tSim, double wall, double* flopGral)//
     f_params = fopen("Parametros.csv", "w");
 
     fprintf(f_params, "Parámetro >>> Valor\n");
-    fprintf(f_params, "Radio del Li >>> %f um\n", RLI);
-    fprintf(f_params, "Separación entre Li >>> %f um\n", DATT);
-    fprintf(f_params, "Longitud de celda en x >>> %f um\n", LONGX);
-    fprintf(f_params, "Longitud de celda en y >>> %f um\n", LONGY);
+    fprintf(f_params, "Separación entre Li >>> %f um\n", DAT);
+    fprintf(f_params, "Longitud de celda en x >>> %f um\n", LX);
+    fprintf(f_params, "Longitud de celda en y >>> %f um\n", LY);
     fprintf(f_params, "Paso temporal >>> %f ms\n", DT);
     fprintf(f_params, "Coeficiente de difusión >>> %f um^2/s\n", D);
-    fprintf(f_params, "Li libre siempre presente >>> %d\n", NM);
-    fprintf(f_params, "Li depositado inicial >>> %d\n", N0);
-    fprintf(f_params, "Li depositado final >>> %d\n", N0MAX);
+    fprintf(f_params, "Li libre siempre presente >>> %d\n", NL);
+    fprintf(f_params, "Li depositado inicial >>> %d\n", ND);
+    fprintf(f_params, "Li depositado final >>> %d\n", NMAX);
     fprintf(f_params, "Tiempo simulado >>> %f ms\n", tSim);
     fprintf(f_params, "WALL TIME >>> %f s\n", wall);
     // fprintf(f_params, "GFLOPS nuetral() >>> %f\n", *flopNeu);
--- dendritas.c
+++ dendritas.c
@@ -13,24 +13,24 @@ int main()
     double tSim = 0.0;
     double* flopGral = (double*)malloc(sizeof(double));
     // double* flopNeu = (double*)malloc(sizeof(double));
-    double* lib = (double*)malloc(2 * NM * sizeof(double));
-    double* dep = (double*)malloc(2 * N0MAX * sizeof(double));
+    double* lib = (double*)malloc(2 * NL * sizeof(double));
+    double* dep = (double*)malloc(2 * NMAX * sizeof(double));
     int* count = (int*)malloc(sizeof(int));
     srand(SEED);
-    *count = N0;
+    *count = ND;
 
     init(lib, dep);
-    printf(">>> Hay %d Li depositados. Progreso = 0 %%.\n", N0);
+    printf(">>> Hay %d Li depositados. Progreso = 0 %%.\n", ND);
 
     double start = wtime();
-    while (*(count) != N0MAX) {
+    while (*(count) != NMAX) {
         move(lib, dep, count, flopGral);//, flopNeu);
         tSim += DT; // sum >> 1
         // *flopGral += *flopNeu;
 
         prog++;
         if (prog % 10000 == 0) {
-            printf(">>> Hay %d Li depositados. Progreso = %d %%.\n", (*count), 100 * ((*count) - N0) / (N0MAX - N0));
+            printf(">>> Hay %d Li depositados. Progreso = %d %%.\n", (*count), 100 * ((*count) - ND) / (NMAX - ND));
         }
     }
     double wall = wtime() - start; // res >> 1
@@ -39,7 +39,7 @@ int main()
     *flopGral *= 0.000000001 / wall;
 
     end(lib, dep, tSim, wall, flopGral);//, flopNeu);
-    printf("Hay un total de %d Li depositados.\n", N0MAX);
+    printf("Hay un total de %d Li depositados.\n", NMAX);
     printf("Tiempo simulando = %.2f ms\n", tSim);
     printf("WALL TIME = %.2f s\n", wall);
     // printf("GFLOPS neutral() = %f\n", *flopNeu);
